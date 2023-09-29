45dd5d7fcbe3c14fafff365e02c4f6df66daa772
Rumasultana23 <romaproma23@gmail.com>
2023-09-29 11:06:46 +0600

    added favorite page part

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
-import PhoneCard from "../Phone/PhoneCard";
+// import PhoneCard from "../Phone/PhoneCard";
+import FavoritesCard from "./FavoritesCard";
 
 
 const Favorites = () => {
 
     const [favorites, setFavorites] = useState([]);
-    const [noFound, setNoFound] = useState('')
+    const [noFound, setNoFound] = useState('');
+
+    const [isShowMore, setIsShowMore] =useState(false);
+
 
     useEffect(() => {
         const favoriteItems = JSON.parse(localStorage.getItem('favorites'));
@@ -18,15 +22,26 @@ const Favorites = () => {
         }
     }, []);
 
+    const handleRemove = () => {
+        localStorage.clear();
+        setFavorites([]);
+        setNoFound("No data Found");
+    };
 
     return (
-        <div>
+        <div> 
             {noFound ? <p className="h-[80vh] flex justify-center items-center">{noFound}</p> : <div>
-            <div className="grid grid-cols-2 gap-5">
+
                 {
-                    favorites?.map(phone => <PhoneCard key={phone.id} phone={phone}></PhoneCard>)
+                    favorites.length > 0 && <button onClick={() => handleRemove()} className="px-5 bg-green-200 block mx-auto font-semibold py-3 rounded">Delete All Favorites</button>
                 }
-            </div>
+                <div className="grid grid-cols-2 gap-5 py-10">
+                    {
+                       isShowMore? favorites?.map(phone => <FavoritesCard key={phone.id} phone={phone}></FavoritesCard>) : favorites?.slice(0,2).map(phone => <FavoritesCard key={phone.id} phone={phone}></FavoritesCard>)
+                    }
+                </div>
+
+                {favorites.length > 2 && <button onClick={() => setIsShowMore(!isShowMore)} className="px-5 bg-green-200 block mx-auto font-semibold py-3 rounded">{isShowMore ? "See Less" : "See More"}</button>} 
             </div>}
         </div>
     );
