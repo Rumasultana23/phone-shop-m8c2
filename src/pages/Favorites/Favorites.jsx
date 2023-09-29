510a32b6bbc529f2e4b5c8e97b40f40d4b14fa69
Rumasultana23 <romaproma23@gmail.com>
2023-09-29 11:48:39 +0600

    added rating in home page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -10,12 +10,17 @@ const Favorites = () => {
 
     const [isShowMore, setIsShowMore] =useState(false);
 
+    const [totalPrice, setTotalPrice] = useState(0);
+
 
     useEffect(() => {
         const favoriteItems = JSON.parse(localStorage.getItem('favorites'));
 
         if (favoriteItems) {
             setFavorites(favoriteItems);
+
+            const total = favoriteItems.reduce((preValue, currentValue)=> preValue + currentValue.price, 0);
+            setTotalPrice(total);
         }
         else {
             setNoFound("No data Found");
@@ -33,8 +38,12 @@ const Favorites = () => {
             {noFound ? <p className="h-[80vh] flex justify-center items-center">{noFound}</p> : <div>
 
                 {
-                    favorites.length > 0 && <button onClick={() => handleRemove()} className="px-5 bg-green-200 block mx-auto font-semibold py-3 rounded">Delete All Favorites</button>
-                }
+                    favorites.length > 0 && (
+                        <div>
+                            <button onClick={() => handleRemove()} className="px-5 bg-green-200 block mx-auto font-semibold py-3 rounded">Delete All Favorites</button>
+                            <h1>Total Price: $ {totalPrice}</h1>
+                        </div>
+                    )}
                 <div className="grid grid-cols-2 gap-5 py-10">
                     {
                        isShowMore? favorites?.map(phone => <FavoritesCard key={phone.id} phone={phone}></FavoritesCard>) : favorites?.slice(0,2).map(phone => <FavoritesCard key={phone.id} phone={phone}></FavoritesCard>)
